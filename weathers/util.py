b0b9a6984d54ec9d4fe3099814a4d62b39951223
huzhongzhong <you@example.com>
2015-11-21 22:37:39 +0800

    support python 2.x

--- weathers/util.py
+++ weathers/util.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 
+_ver = sys.version_info
+
+#:Python 2.x
+is_py2 = (_ver[0] == 2)
+
+if is_py2:
+    import codecs
+    open = codecs.open
 
 def get_city_code(province, city, county):
     """
@@ -15,13 +24,13 @@ def get_city_code(province, city, county):
         city_ids = {}
         with open(city_id_path, encoding="utf-8") as f:
             for line in f:
+                line = line.encode('utf-8') if is_py2 else line
                 city_id, county, city, province = line.split(",")
                 key = province.strip() + city.strip() + county.strip()
                 city_ids[key] = city_id.strip()
         return city_ids
 
     city_ids = get_city_ids()
-
     key = province.strip() + city.strip() + county.strip()
     if key in city_ids:
         return city_ids[key]
